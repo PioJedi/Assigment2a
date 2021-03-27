cb5ca7deb466d3ea90de88b1a8d083ba15e306c8
PioJedi <81320805+PioJedi@users.noreply.github.com>
2021-03-26 17:16:59 -0700

    Create average.py

---
+++ average.py
@@ -0,0 +1,11 @@
+print("Please enter five numbers.")
+num = 0
+num += int(input())
+num += int(input())
+num += int(input())
+num += int(input())
+num += int(input())
+average = num/5
+
+print("The average of those numbers is:")
+print(average)
